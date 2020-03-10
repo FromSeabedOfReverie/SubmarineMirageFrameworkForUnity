a39987ba74f007cb146a9dbe5e8ae0b2e12fd132
SirenSingsCurse <siren.sings.curse@gmail.com>
2020-03-10 21:52:58 +0900

    新規FSMに、活動状態変更時、状態をExitで非活動化、Enterで活動化するように、修正。

--- Assets/SubmarineMirageFrameworkForUnity/Scripts/System/FSM/New/FiniteStateMachine.cs
+++ Assets/SubmarineMirageFrameworkForUnity/Scripts/System/FSM/New/FiniteStateMachine.cs
@@ -12,8 +12,6 @@ namespace SubmarineMirageFramework.FSM.New {
 	using UniRx;
 	using UniRx.Async;
 	using KoganeUnityLib;
-	using Process;
-	using MultiEvent;
 	using Extension;
 	using Utility;
 	using Debug;
@@ -26,19 +24,30 @@ namespace SubmarineMirageFramework.FSM.New {
 		where TOwner : IFiniteStateMachineOwner<TFSM>
 		where TFSM : IFiniteStateMachine
 	{
-		public enum RunState {
+		enum ActiveState {
+			Enabling,
+			Enabled,
+			Disabling,
+			Disabled,
+		}
+		enum RunState {
 			Enter,
 			Update,
 			Exit,
 		}
 		protected TOwner _owner	{ get; private set; }
+		string _name;
+		public bool _isActive => _activeState == ActiveState.Enabled;
+		ActiveState _activeState;
+		RunState _runState;
+
 		readonly Dictionary< Type, State<TOwner, TFSM> > _states = new Dictionary< Type, State<TOwner, TFSM> >();
 		public State<TOwner, TFSM> _state	{ get; private set; }
 		protected State<TOwner, TFSM> _nextState;
-		public RunState _runState	{ get; private set; }
+
 		CancellationTokenSource _asyncCanceler = new CancellationTokenSource();
-		string _name;
-		public bool _isActive	{ get; private set; }
+		CompositeDisposable _updateDisposer = new CompositeDisposable();
+
 
 		public FiniteStateMachine( TOwner owner, State<TOwner, TFSM>[] states ) {
 			_owner = owner;
@@ -54,53 +63,55 @@ namespace SubmarineMirageFramework.FSM.New {
 						.Select( pair => pair.Value )
 						.Select( async state => await state._initializeEvent.Invoke( cancel ) )
 				);
-				_isActive = true;
-				await ChangeState( cancel, _states.First().Key );
+				_nextState = _states.First().Value;
 			} );
 
 			_owner._enableEvent.AddLast( _name, async cancel => {
-				if ( _isActive )	{ return; }
-				_state._updateEvent.Invoke( _asyncCanceler.Token ).Forget();
-				_isActive = true;
-				await UniTaskUtility.DontWait( cancel );
+				_activeState = ActiveState.Enabling;
+				ChangeStateSub().Forget();
+				await UniTaskUtility.WaitWhile( cancel, () => _activeState == ActiveState.Enabling );
 			} );
+
 			_owner._disableEvent.AddLast( _name, async cancel => {
-				await UniTaskUtility.WaitWhile( cancel, () => _runState != RunState.Update );
+				_activeState = ActiveState.Disabling;
+				if ( _runState == RunState.Update ) {
+					ChangeStateSub().Forget();
+				}
+				await UniTaskUtility.WaitWhile( cancel, () => _activeState == ActiveState.Disabling );
 				StopAsync();
-				_isActive = false;
 			} );
 
 			_owner._updateEvent.AddLast( _name )
 				.Where( _ => _runState == RunState.Update )
 				.Subscribe( _ => _state._updateDeltaEvent.Invoke() );
+
 			_owner._fixedUpdateEvent.AddLast( _name )
 				.Where( _ => _runState == RunState.Update )
 				.Subscribe( _ => _state._fixedUpdateDeltaEvent.Invoke() );
+
 			_owner._lateUpdateEvent.AddLast( _name )
 				.Where( _ => _runState == RunState.Update )
 				.Subscribe( _ => _state._lateUpdateDeltaEvent.Invoke() );
 #if DEVELOP
 			Observable.EveryUpdate()
-				.TakeWhile( _ => !_states.IsEmpty() )
 				.Subscribe( _ => {
 					DebugDisplay.s_instance.Add( $"{_owner.GetAboutName()}.{_name}" );
 					DebugDisplay.s_instance.Add( $"_state : {_state.GetAboutName()}.{_runState}" );
 					DebugDisplay.s_instance.Add(
 						$"_nextState : {( _nextState == null ? "null" : _nextState.GetAboutName() )}"
 					);
-				} );
+				} )
+				.AddTo( _updateDisposer );
 #endif
 		}
+
 
 		public async UniTask ChangeState<TState>( CancellationToken cancel ) {
 			await ChangeState( cancel, typeof( TState ) );
 		}
 
+
 		public async UniTask ChangeState( CancellationToken cancel, Type stateType ) {
-			if ( !_isActive ) {
-				Log.Error( $"非活動中は状態遷移が不可能 : {stateType}", Log.Tag.FSM );
-				return;
-			}
 			if ( !_states.ContainsKey( stateType ) ) {
 				Log.Error( $"未定義状態へ遷移 : {stateType}", Log.Tag.FSM );
 				return;
@@ -112,7 +123,7 @@ namespace SubmarineMirageFramework.FSM.New {
 			}
 			_nextState = next;
 
-			if ( _state == null || _runState == RunState.Update ) {
+			if ( _isActive && ( _state == null || _runState == RunState.Update ) ) {
 				Log.Debug( $"{_name} ChangeStateSub" );
 				ChangeStateSub().Forget();
 			}
@@ -121,30 +132,41 @@ namespace SubmarineMirageFramework.FSM.New {
 			Log.Debug( $"待機終了 {_state.GetAboutName()}.{_runState}" );
 		}
 
+
 		async UniTask ChangeStateSub() {
 			StopAsync();
 
 			_runState = RunState.Exit;
-			if ( _state != null ) {
+			if ( _activeState != ActiveState.Enabling && _state != null ) {
 				await _state._exitEvent.Invoke( _asyncCanceler.Token );
 			}
-			_state = _nextState;
-			_nextState = null;
-
+			if ( _activeState == ActiveState.Disabling ) {
+				_activeState = ActiveState.Disabled;
+				return;
+			}
+			if ( _nextState != null ) {
+				_state = _nextState;
+				_nextState = null;
+			}
+			if ( _activeState == ActiveState.Enabling ) {
+				_activeState = ActiveState.Enabled;
+			}
 			_runState = RunState.Enter;
 			await _state._enterEvent.Invoke( _asyncCanceler.Token );
 
-			if ( _nextState == null ) {
+			if ( _activeState == ActiveState.Disabling || _nextState != null ) {
+				await ChangeStateSub();
+			} else {
 				_runState = RunState.Update;
 				_state._updateEvent.Invoke( _asyncCanceler.Token ).Forget();
-			} else {
-				await ChangeStateSub();
 			}
 		}
+
 
 		protected virtual bool IsPossibleChangeState( State<TOwner, TFSM> changeState ) {
 			return true;
 		}
+
 
 		void StopAsync() {
 			_asyncCanceler.Cancel();
@@ -152,16 +174,20 @@ namespace SubmarineMirageFramework.FSM.New {
 			_asyncCanceler = new CancellationTokenSource();
 		}
 
+
 		public override string ToString() {
 			return this.ToDeepString();
 		}
+
 
 		public virtual void Dispose() {
 			_states.ForEach( pair => pair.Value.Dispose() );
 			_states.Clear();
 			_asyncCanceler.Cancel();
 			_asyncCanceler.Dispose();
+			_updateDisposer.Dispose();
 		}
+
 
 		~FiniteStateMachine() {
 			Dispose();
