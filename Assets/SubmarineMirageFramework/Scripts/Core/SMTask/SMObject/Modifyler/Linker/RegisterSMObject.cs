b0c953020305413d051acb9df2dec916494831c3
SirenSingsCurse <siren.sings.curse@gmail.com>
2020-08-19 19:36:50 +0900

    RegisterSMObjectに、デバッグ表示を追加し、テストを追加、デバッグ完了。

--- Assets/SubmarineMirageFramework/Scripts/Core/SMTask/SMObject/Modifyler/Linker/RegisterSMObject.cs
+++ Assets/SubmarineMirageFramework/Scripts/Core/SMTask/SMObject/Modifyler/Linker/RegisterSMObject.cs
@@ -4,6 +4,7 @@
 //		Released under the MIT License :
 //			https://github.com/FromSeabedOfReverie/SubmarineMirageFrameworkForUnity/blob/master/LICENSE
 //---------------------------------------------------------------------------------------------------------
+//#define TestSMTaskModifyler
 namespace SubmarineMirage.SMTask.Modifyler {
 	using System;
 	using Cysharp.Threading.Tasks;
@@ -12,7 +13,9 @@ namespace SubmarineMirage.SMTask.Modifyler {
 	using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
 
 
+
 	// TODO : コメント追加、整頓
+
 
 
 	public class RegisterSMObject : SMObjectModifyData {
@@ -25,17 +28,31 @@ namespace SubmarineMirage.SMTask.Modifyler {
 			}
 		}
 
+
 		public override void Cancel() {
 			_object.Dispose();
+#if TestSMTaskModifyler
+			Log.Debug( $"{nameof( Cancel )} : {this}" );
+#endif
 		}
 
 
 		public override async UniTask Run() {
 			if ( _object._lifeSpan == SMTaskLifeSpan.Forever && _object._owner != null ) {
 				UnitySceneManager.MoveGameObjectToScene( _object._owner, _object._scene._scene );
-//				Log.Debug( $"MoveGameObjectToScene : {_object._owner}, {_object._scene._scene}" );
+#if TestSMTaskModifyler
+				Log.Debug( string.Join( "\n",
+					$"{SMTaskLifeSpan.Forever}シーン移動 :",
+					$"{_object?.ToLineString()}",
+					$"{_object?._scene}"
+				) );
 			} else {
-//				Log.Debug( "Dont MoveGameObjectToScene" );
+				Log.Debug( string.Join( "\n",
+					$"シーン移動なし :",
+					$"{_object?.ToLineString()}",
+					$"{_object?._scene}"
+				) );
+#endif
 			}
 			RegisterObject();
 
@@ -46,23 +63,46 @@ namespace SubmarineMirage.SMTask.Modifyler {
 		async UniTask SetRunObject() {
 			switch ( _object._type ) {
 				case SMTaskType.DontWork:
-					// 生成直後だと、継承先コンストラクタ前に実行されてしまう為、1フレーム待機
-					await UTask.NextFrame( _object._asyncCanceler );
+					// Mono未使用の場合、生成直後だと、継承先コンストラクタ前に実行されてしまう為、1フレーム待機
+					if ( _object._owner == null ) {
+#if TestSMTaskModifyler
+						Log.Debug( $"待機開始 : {this}" );
+#endif
+						await UTask.NextFrame( _object._asyncCanceler );
+#if TestSMTaskModifyler
+						Log.Debug( $"待機終了 : {this}" );
+#endif
+					}
 					RunStateSMObject.RunOrRegister( _object, SMTaskRanState.Creating );
+					break;
 
-					return;
 				case SMTaskType.Work:
 				case SMTaskType.FirstWork:
 					if ( _object._objects._isEnter ) {
-						// 生成直後だと、継承先コンストラクタ前に実行されてしまう為、1フレーム待機
-						await UTask.NextFrame( _object._asyncCanceler );
+						// Mono未使用の場合、生成直後だと、継承先コンストラクタ前に実行されてしまう為、1フレーム待機
+						if ( _object._owner == null ) {
+#if TestSMTaskModifyler
+							Log.Debug( $"待機開始 : {this}" );
+#endif
+							await UTask.NextFrame( _object._asyncCanceler );
+#if TestSMTaskModifyler
+							Log.Debug( $"待機終了 : {this}" );
+#endif
+						}
 						RunStateSMObject.RunOrRegister( _object, SMTaskRanState.Creating );
 						RunStateSMObject.RunOrRegister( _object, SMTaskRanState.Loading );
 						RunStateSMObject.RunOrRegister( _object, SMTaskRanState.Initializing );
 						_object._top._modifyler.Register( new RunActiveSMObject( _object ) );
 					}
-					return;
+					break;
 			}
+#if TestSMTaskModifyler
+			Log.Debug( string.Join( "\n",
+				$"{nameof( SetRunObject )} :",
+				$"{nameof( _object._objects._isEnter )} : {_object._objects._isEnter}",
+				$"{nameof( _object._top._modifyler )} : {_object._top._modifyler}"
+			) );
+#endif
 		}
 	}
 }
--- Assets/Test/Scripts/SMTask/SMObject/Modifyler/Linker/TestRegisterSMObject.cs
+++ Assets/Test/Scripts/SMTask/SMObject/Modifyler/Linker/TestRegisterSMObject.cs
@@ -10,6 +10,7 @@ namespace SubmarineMirage.TestSMTask.Modifyler {
 	using NUnit.Framework;
 	using UnityEngine.TestTools;
 	using UnityEngine;
+	using Cysharp.Threading.Tasks;
 	using KoganeUnityLib;
 	using UTask;
 	using SMTask;
@@ -31,7 +32,10 @@ namespace SubmarineMirage.TestSMTask.Modifyler {
 			_createEvent.AddLast( async canceler => {
 				Log.Debug( $"start {nameof( Create )}{_testName}" );
 				switch ( _testName ) {
-					case nameof( TestRegisterAddChildObject ):	CreateTestRegisterAddChildObject();	break;
+					case nameof( TestMoveGameObjectToScene ):	CreateTestMoveGameObjectToScene();	break;
+					case nameof( TestSetRunObject ):			CreateTestSetRunObject();			break;
+					case nameof( TestCancel ):					CreateTestCancel();					break;
+					case nameof( TestError ):					CreateTestError();					break;
 				}
 				Log.Debug( $"end {nameof( Create )}{_testName}" );
 
@@ -40,26 +44,173 @@ namespace SubmarineMirage.TestSMTask.Modifyler {
 		}
 
 
-
 /*
-		・登録追加テスト
-		RegisterObject、AddObject、AddChildObject、を確認
+		・シーン移動テスト
+		Run、SMTaskLifeSpan.InScene、SMTaskLifeSpan.Forever、確認
 */
-		void CreateTestRegisterAddChildObject() {}
+		void CreateTestMoveGameObjectToScene() {}
 
 		[UnityTest] [Timeout( int.MaxValue )]
-		public IEnumerator TestRegisterAddChildObject() => From( async () => {
-			Log.Debug( $"{nameof( TestRegisterAddChildObject )}" );
+		public IEnumerator TestMoveGameObjectToScene() => From( async () => {
+			Log.Debug( $"{nameof( TestMoveGameObjectToScene )}" );
+
+			await UTask.NextFrame( _asyncCanceler );
 
 
-			Log.Debug( $"・RegisterObjectのテスト" );
-			3.Times( () => {
+			Log.Debug( $"・{SMTaskLifeSpan.InScene}のテスト" );
+			new B1();
+			await UTask.NextFrame( _asyncCanceler );
+
+			var b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M1" );
+			new SMObject( b.gameObject, new [] { b }, null );
+			await UTask.NextFrame( _asyncCanceler );
+
+
+			Log.Debug( $"・{SMTaskLifeSpan.Forever}のテスト" );
+			new B4();
+			await UTask.NextFrame( _asyncCanceler );
+
+			b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M4" );
+			new SMObject( b.gameObject, new [] { b }, null );
+			await UTask.NextFrame( _asyncCanceler );
+
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・種類別登録テスト
+		SetRunObject、SMTaskType.DontWork、SMTaskType.Work、SMTaskType.FirstWork、確認
+*/
+		void CreateTestSetRunObject() {}
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestSetRunObject() => From( async () => {
+			Log.Debug( $"{nameof( TestSetRunObject )}" );
+
+			await UTask.NextFrame( _asyncCanceler );
+
+
+			Log.Debug( $"・{SMTaskType.DontWork}のテスト" );
+			new B1();
+			await UTask.NextFrame( _asyncCanceler );
+
+			var b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M1" );
+			new SMObject( b.gameObject, new [] { b }, null );
+			await UTask.NextFrame( _asyncCanceler );
+
+
+			Log.Debug( $"・{SMTaskType.Work}のテスト" );
+			new B2();
+			await UTask.NextFrame( _asyncCanceler );
+
+			b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M2" );
+			new SMObject( b.gameObject, new [] { b }, null );
+			await UTask.NextFrame( _asyncCanceler );
+
+
+			Log.Debug( $"・{SMTaskType.FirstWork}のテスト" );
+			new B3();
+			await UTask.NextFrame( _asyncCanceler );
+
+			b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M3" );
+			new SMObject( b.gameObject, new [] { b }, null );
+			await UTask.NextFrame( _asyncCanceler );
+
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・停止テスト
+		Cancel、確認
+*/
+		void CreateTestCancel() {}
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestCancel() => From( async () => {
+			await UTask.NextFrame( _asyncCanceler );
+			Log.Debug( $"{nameof( TestCancel )}" );
+
+
+			var o = new B1( true )._object;
+			o._top = o;
+			o._scene = SceneManager.s_instance._fsm._scene;
+			var d = new RegisterSMObject( o );
+			d.Cancel();
+
+			o = new B1( true )._object;
+			o._top = o;
+			o._scene = SceneManager.s_instance._fsm._scene;
+			d = new RegisterSMObject( o );
+			d.Run().Forget();
+			d.Cancel();
+			await UTask.NextFrame( _asyncCanceler );
+
+			o = new B1( true )._object;
+			o._top = o;
+			o._scene = SceneManager.s_instance._fsm._scene;
+			d = new RegisterSMObject( o );
+			await d.Run();
+			d.Cancel();
+
+			o = new B1( true )._object;
+
+
+			var b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M1" );
+			o = new SMObject( b.gameObject, new [] { b }, null, true );
+			o._top = o;
+			o._scene = SceneManager.s_instance._fsm._scene;
+			d = new RegisterSMObject( o );
+			d.Cancel();
+
+			b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M1" );
+			o = new SMObject( b.gameObject, new [] { b }, null, true );
+			o._top = o;
+			o._scene = SceneManager.s_instance._fsm._scene;
+			d = new RegisterSMObject( o );
+			d.Run().Forget();
+			d.Cancel();
+			await UTask.NextFrame( _asyncCanceler );
+
+			b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M1" );
+			o = new SMObject( b.gameObject, new [] { b }, null, true );
+			o._top = o;
+			o._scene = SceneManager.s_instance._fsm._scene;
+			d = new RegisterSMObject( o );
+			await d.Run();
+			d.Cancel();
+
+			b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M1" );
+			o = new SMObject( b.gameObject, new [] { b }, null, true );
+
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・エラーテスト
+		RegisterSMObject、top以外、確認
+*/
+		void CreateTestError() {}
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestError() => From( async () => {
+			await UTask.NextFrame( _asyncCanceler );
+			Log.Debug( $"{nameof( TestError )}" );
+
+			try {
+				new RegisterSMObject( null );
+			} catch ( Exception e )	{ Log.Error( e ); }
+
+			try {
 				var b = (SMMonoBehaviour)TestSMBehaviourUtility.CreateBehaviours( "M1" );
 				var o = new SMObject( b.gameObject, new [] { b }, null, true );
-				o._type = SMTaskType.DontWork;
-				o._scene = SceneManager.s_instance._fsm._scene;
-				new LinkData( o ).TestRegisterObject();
-			} );
+				new RegisterSMObject( o );
+			} catch ( Exception e )	{ Log.Error( e ); }
 
 			await UTask.Never( _asyncCanceler );
 		} );
