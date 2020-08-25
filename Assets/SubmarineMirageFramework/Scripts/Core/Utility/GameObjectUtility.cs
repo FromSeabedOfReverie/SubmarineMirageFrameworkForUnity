0ffe677f09881aed9caf007dbec9d1c98eed7449
SirenSingsCurse <siren.sings.curse@gmail.com>
2020-08-25 19:37:09 +0900

    GameObjectUtilityを、ListからIEnumerableに変更。 取得方法も、遅延実行に対応。 テストを、最新記述に対応し、テスト完了した。

--- Assets/SubmarineMirageFramework/Scripts/Core/Utility/GameObjectUtility.cs
+++ Assets/SubmarineMirageFramework/Scripts/Core/Utility/GameObjectUtility.cs
@@ -21,75 +21,89 @@ namespace SubmarineMirage.Utility {
 	public static class GameObjectUtility {
 		///------------------------------------------------------------------------------------------------
 		/// <summary>
-		/// ● 指定層の子供達のゲーム物を全取得
+		/// ● 指定層の子達のゲーム物を全取得
 		/// </summary>
 		///------------------------------------------------------------------------------------------------
-		public static List<GameObject> GetChildrenInLayer( GameObject gameObject, LayerManager.Name layer ) {
+		public static IEnumerable<GameObject> GetChildrenInLayer( GameObject gameObject, LayerManager.Name layer ) {
 			var id = LayerManager.s_instance.ToInt( layer );
 			return gameObject.GetChildren()
-				.Where( go => go.layer == id )
-				.ToList();
+				.Where( go => go.layer == id );
 		}
 		///------------------------------------------------------------------------------------------------
 		/// <summary>
-		/// ● 1階層までの、子供達の、部品達を取得
+		/// ● 1階層までの、子達の、部品達を取得
 		/// </summary>
 		///------------------------------------------------------------------------------------------------
-		public static List<T> GetComponentsInChildrenUntilOneHierarchy<T>( GameObject gameObject,
-																			bool isIncludeInactive = false
+		public static IEnumerable<T> GetComponentsInChildrenUntilOneHierarchy<T>( GameObject gameObject,
+																					bool isIncludeInactive = false
 		) {
-			var results = new List<T>();
 			// 自身が非活動中の場合、未処理
-			if ( !isIncludeInactive && !gameObject.activeInHierarchy )	{ return results; }
+			if ( !isIncludeInactive && !gameObject.activeInHierarchy )	{ yield break; }
 
-			var transforms = new List<Transform> { gameObject.transform };
-			// 自身の同一階層達が無くなるまで、再帰処理
-			while ( !transforms.IsEmpty() ) {
-				// 自身の、同一階層達の、全子供達の順で走査
-				var children = new List<Transform>();
-				transforms.ForEach( transform => {
-					foreach ( Transform child in transform ) {
-						// 子が活動中の場合
-						if ( isIncludeInactive || child.gameObject.activeInHierarchy ) {
-							var cs = child.GetComponents<T>();
-							// 部品が無い場合、更に子供を処理
-							if ( !cs.IsEmpty() )	{ results.Add( cs ); }
-							else					{ children.Add( child ); }
-						}
-					}
-				} );
-				transforms = children;	// 子供達を自身の階層とし、再帰処理
+			// 子階層から、走査開始を設定
+			var currents = new Queue<Transform>();
+			foreach ( Transform child in gameObject.transform ) {
+				currents.Enqueue( child );
 			}
 
-			return results;
+			// 階層達が存在する限り、再帰処理
+			while ( !currents.IsEmpty() ) {
+				var current = currents.Dequeue();
+
+				// 非活動時に取得せず、子が非活動中の場合、未取得
+				if ( !isIncludeInactive && !current.gameObject.activeInHierarchy )	{ continue; }
+
+				// 部品達が存在する場合、部品達を返す
+				var cs = current.GetComponents<T>();
+				if ( !cs.IsEmpty() ) {
+					foreach ( var c in cs )	{ yield return c; }
+
+				// 部品が無い場合、子階層を追加
+				} else {
+					foreach ( Transform child in current ) {
+						currents.Enqueue( child );
+					}
+				}
+			}
 		}
 		///------------------------------------------------------------------------------------------------
 		/// <summary>
 		/// ● 1階層までの、親の、部品達を取得
 		/// </summary>
 		///------------------------------------------------------------------------------------------------
-		public static List<T> GetComponentsInParentUntilOneHierarchy<T>( GameObject gameObject,
-																			bool isIncludeInactive = false,
-																			bool isGetOnlyOne = false
+		public static IEnumerable<T> GetComponentsInParentUntilOneHierarchy<T>( GameObject gameObject,
+																				bool isIncludeInactive = false,
+																				bool isGetOnlyOne = false
 		) {
-			var results = new List<T>();
 			// 自身が非活動中の場合、未処理
-			if ( !isIncludeInactive && !gameObject.activeInHierarchy )	{ return results; }
+			if ( !isIncludeInactive && !gameObject.activeInHierarchy )	{ yield break; }
 
+			// 親が存在する限り、再帰処理
 			var parent = gameObject.transform.parent;
-			// 親が無いか、部品が存在するまで、再帰処理
-			while ( parent != null && results.IsEmpty() ) {
-				// 親が活動中の場合、部品達を取得
-				if ( isIncludeInactive || parent.gameObject.activeInHierarchy ) {
-					if ( isGetOnlyOne )	{ results.Add( parent.GetComponent<T>() ); }
-					else				{ results.Add( parent.GetComponents<T>() ); }
-					parent = parent.parent;	// 部品達が無い場合に備え、親を再指定し、再帰処理
-				} else {
-					parent = null;	// 親が非活動中の場合、再帰終了
-				}
-			}
+			while ( parent != null ) {
+				// 非活動時に取得せず、親が非活動中の場合、再帰終了
+				if ( !isIncludeInactive && !parent.gameObject.activeInHierarchy )	{ yield break; }
 
-			return results;
+				// 1つだけ取得する場合
+				if ( isGetOnlyOne ) {
+					var c = parent.GetComponent<T>();
+					if ( c != null ) {
+						yield return c;
+						yield break;
+					}
+
+				// 複数取得する場合
+				} else {
+					var cs = parent.GetComponents<T>();
+					if ( !cs.IsEmpty() ) {
+						foreach ( var c in cs )	{ yield return c; }
+						yield break;
+					}
+				}
+
+				// 部品が無い場合、親を再指定し、再帰処理
+				parent = parent.parent;
+			}
 		}
 		///------------------------------------------------------------------------------------------------
 		/// <summary>
@@ -98,10 +112,8 @@ namespace SubmarineMirage.Utility {
 		///------------------------------------------------------------------------------------------------
 		public static T GetComponentInParentUntilOneHierarchy<T>( GameObject gameObject,
 																	bool isIncludeInactive = false
-		) {
-			return GetComponentsInParentUntilOneHierarchy<T>( gameObject, isIncludeInactive, true )
+		) => GetComponentsInParentUntilOneHierarchy<T>( gameObject, isIncludeInactive, true )
 				.FirstOrDefault();
-		}
 		///------------------------------------------------------------------------------------------------
 		/// <summary>
 		/// ● 指定名ゲーム物から、部品を取得
--- Assets/Test/Scripts/Utility/TestGameObjectUtility.cs
+++ Assets/Test/Scripts/Utility/TestGameObjectUtility.cs
@@ -5,79 +5,131 @@
 //			https://github.com/FromSeabedOfReverie/SubmarineMirageFrameworkForUnity/blob/master/LICENSE
 //---------------------------------------------------------------------------------------------------------
 namespace SubmarineMirage.TestUtility {
+	using System.Linq;
 	using System.Collections;
 	using NUnit.Framework;
 	using UnityEngine;
-	using UnityEngine.SceneManagement;
 	using UnityEngine.TestTools;
 	using UniRx;
 	using Cysharp.Threading.Tasks;
 	using KoganeUnityLib;
 	using UTask;
+	using SMTask;
+	using Scene;
 	using Extension;
+	using Utility;
 	using Debug;
 	using Test;
+	using TestSMTask;
+	using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
 
 
 	// TODO : コメント追加、整頓
 
 
+
 	public class TestGameObjectUtility : Test {
-		protected override void Create() {}
+		protected override void Create() {
+			Application.targetFrameRate = 30;
 
+			_createEvent.AddLast( async canceler => {
+				Log.Debug( $"start {nameof( Create )}{_testName}" );
+				switch ( _testName ) {
+					case nameof( TestGetComponentsInParentUntilOneHierarchy ):
+														CreateTestGetComponentsInParentUntilOneHierarchy();	break;
+				}
+				Log.Debug( $"end {nameof( Create )}{_testName}" );
 
-		[UnityTest]
-		[Timeout( int.MaxValue )]
-		public IEnumerator TestGetRootGameObjects() => From( async () => {
-			await SceneManager.LoadSceneAsync( "TestChange1", LoadSceneMode.Additive )
-				.ToUniTask( _asyncCanceler );
-			SceneManager.SetActiveScene( SceneManager.GetSceneByName( "TestChange1" ) );
-			new GameObject( "New" );
-			var gos = SceneManager.GetActiveScene().GetRootGameObjects();
-			gos.ForEach( go => Log.Debug( go ) );
-			await UTask.WaitWhile( _asyncCanceler, () => true );
-		} );
-
-
-		[UnityTest]
-		[Timeout( int.MaxValue )]
-		public IEnumerator TestGetComponentsInParentUntilOneHierarchy() => From( async () => {
-			var top = new GameObject( $"{nameof( TestMono )} top" );
-			top.AddComponent<TestMono>();
-//			top.SetActive( false );
-			var parent = top.transform;
-			GameObject topChild = null;
-			var id = 0;
-
-			3.Times( brotherIndex => {
-				3.Times( hierarchyIndex => {
-					var go = new GameObject();
-					go.name = $"{nameof( TestMono )} {id++}";
-					go.SetParent( parent );
-					parent = go.transform;
-
-					if ( hierarchyIndex == 2 )	{ go.SetActive( false ); }
-					if ( hierarchyIndex <= 0 && brotherIndex == 0 )	{ go.AddComponent<TestMono>(); }
-					if ( hierarchyIndex <= 1 && brotherIndex == 1 )	{ go.AddComponent<TestMono>(); }
-					if ( hierarchyIndex <= 2 && brotherIndex == 2 )	{ go.AddComponent<TestMono>(); }
-					if ( hierarchyIndex == 2 && brotherIndex == 0 )	{ topChild = go; }
-				} );
-				parent = top.transform;
+				await UTask.DontWait();
 			} );
+		}
 
-			var ts = topChild.GetComponentsInParentUntilOneHierarchy<TestMono>( true );
-			ts.ForEach( testHoge => Log.Debug( testHoge.gameObject.name ) );
 
-			await UTask.WaitWhile( _asyncCanceler, () => true );
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestGetRootGameObjects() => From( async () => {
+			await UnitySceneManager.LoadSceneAsync(
+				"TestChange1", UnityEngine.SceneManagement.LoadSceneMode.Additive
+				).ToUniTask( _asyncCanceler );
+			UnitySceneManager.SetActiveScene( UnitySceneManager.GetSceneByName( "TestChange1" ) );
+			new GameObject( "New" );
+			var gos = UnitySceneManager.GetActiveScene().GetRootGameObjects();
+			gos.ForEach( go => Log.Debug( go ) );
+
+			await UTask.Never( _asyncCanceler );
 		} );
 
 
-		public class TestMono : MonoBehaviour {
-			static int s_count;
-			public int _id	{ get; private set; }
-			void Awake() {
-				_id = s_count++;
-			}
-		}
+
+		public void CreateTestGetComponentsInParentUntilOneHierarchy() => TestSMBehaviourUtility.CreateBehaviours(
+			@"
+				M1, M1,
+					M1, M1,
+						false,
+							,
+					M1, M1,
+						false, M1, M1,
+							,
+					,
+						false,
+							M1, M1,
+			"
+		);
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestGetComponentsInParentUntilOneHierarchy() => From( async () => {
+			var top = SceneManager.s_instance.GetBehaviour<M1>().transform;
+
+
+			Log.Debug( "・0番目の最子テスト" );
+			var child = top.GetChild( 0 ).GetChild( 0 ).GetChild( 0 );
+
+			var bs = child.GetComponentsInParentUntilOneHierarchy<M1>( true );
+			Log.Debug( string.Join( "\n",
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} :",
+				string.Join( "\n", bs.Select( b => b.ToLineString() ) )
+			) );
+
+			bs = child.GetComponentsInParentUntilOneHierarchy<M1>( false );
+			Log.Debug( string.Join( "\n",
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} :",
+				string.Join( "\n", bs.Select( b => b.ToLineString() ) )
+			) );
+
+			var be = child.GetComponentInParentUntilOneHierarchy<M1>( true );
+			Log.Debug(
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} : {be?.ToLineString()}" );
+
+			be = child.GetComponentInParentUntilOneHierarchy<M1>( false );
+			Log.Debug(
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} : {be?.ToLineString()}" );
+
+
+			Log.Debug( "・2番目の最子テスト" );
+			child = top.GetChild( 2 ).GetChild( 0 ).GetChild( 0 );
+			bs = child.GetComponentsInParentUntilOneHierarchy<M1>( true );
+			Log.Debug( string.Join( "\n",
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} :",
+				string.Join( "\n", bs.Select( b => b.ToLineString() ) )
+			) );
+
+			bs = child.GetComponentsInParentUntilOneHierarchy<M1>( false );
+			Log.Debug( string.Join( "\n",
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} :",
+				string.Join( "\n", bs.Select( b => b.ToLineString() ) )
+			) );
+
+			be = child.GetComponentInParentUntilOneHierarchy<M1>( true );
+			Log.Debug(
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} : {be?.ToLineString()}" );
+
+			be = child.GetComponentInParentUntilOneHierarchy<M1>( false );
+			Log.Debug(
+				$"{nameof( GameObjectExtension.GetComponentsInParentUntilOneHierarchy )} : {be?.ToLineString()}" );
+
+
+			await UTask.Never( _asyncCanceler );
+		} );
 	}
 }
