e90142c2f2d646804073c4c9252c7b79c4b099b7
SirenSingsCurse <siren.sings.curse@gmail.com>
2020-08-31 20:46:38 +0900

    ChangeParentSMObjectに、デバッグ表示を追加。 テストを追加。

--- Assets/SubmarineMirageFramework/Scripts/Core/SMTask/SMObject/Modifyler/Linker/ChangeParentSMObject.cs
+++ Assets/SubmarineMirageFramework/Scripts/Core/SMTask/SMObject/Modifyler/Linker/ChangeParentSMObject.cs
@@ -8,7 +8,6 @@
 namespace SubmarineMirage.SMTask.Modifyler {
 	using System;
 	using System.Linq;
-	using System.Collections.Generic;
 	using UnityEngine;
 	using Cysharp.Threading.Tasks;
 	using Extension;
@@ -26,6 +25,7 @@ namespace SubmarineMirage.SMTask.Modifyler {
 		bool _isWorldPositionStays;
 
 
+
 		public ChangeParentSMObject( SMObject smObject, Transform parent, bool isWorldPositionStays )
 			: base( smObject )
 		{
@@ -39,7 +39,20 @@ namespace SubmarineMirage.SMTask.Modifyler {
 		public override void Cancel() {}
 
 
+
 		public override async UniTask Run() {
+#if TestSMTaskModifyler
+			Log.Debug( $"{nameof( Run )} : start\n{this}" );
+			var lastTopDebug = _object._top;
+			Log.Debug( string.Join( "\n",
+				$"{nameof( _object )} : {_object}",
+				$"{nameof( lastTopDebug )} : {lastTopDebug}",
+				$"lastObjects :",
+				string.Join( "\n", lastTopDebug._objects._objects.Select( pair =>
+					$"    {pair.Key} : {pair.Value?.ToLineString()}" ) )
+			) );
+
+#endif
 			_object._owner.transform.SetParent( _parent, _isWorldPositionStays );
 
 			UnLinkObject( _object );
@@ -50,21 +63,56 @@ namespace SubmarineMirage.SMTask.Modifyler {
 			if ( parent != null )	{ AddChildObject( parent, _object ); }
 			else					{ RegisterObject(); }
 
-			var top = _object._top;
+			var lastTop = _object._top;
 			SetTopObject( _object );
 
-			top._modifyler.ReRegister( _object );
+// TODO : 親も子も、順番を保ったまま、移動しないといけない
+// TODO : ChangeActiveSMObjectを待機中に、移動後のトップがどんどん次を実行して、待機されない
+			lastTop._modifyler.ReRegister( _object );
 
 			if ( _object._parent != null && _object._owner.activeSelf ) {
+#if TestSMTaskModifyler
+				Log.Debug( string.Join( "\n",
+					$"{nameof( ChangeActiveSMObject )} : start",
+					$"isActive : {_object._owner.activeSelf}",
+					$"isParentActive : {_object._parent._owner.activeInHierarchy}",
+					$"{nameof( _object )} : {_object}"
+				) );
+#endif
 				var isParentActive = _object._parent._owner.activeInHierarchy;
 				await new ChangeActiveSMObject( _object, isParentActive, false ).Run();
+#if TestSMTaskModifyler
+				Log.Debug( string.Join( "\n",
+					$"{nameof( ChangeActiveSMObject )} : end",
+					$"isActive : {_object._owner.activeSelf}",
+					$"isParentActive : {_object._parent._owner.activeInHierarchy}",
+					$"{nameof( _object )} : {_object}"
+				) );
+#endif
 			}
+
+#if TestSMTaskModifyler
+			Log.Debug( string.Join( "\n",
+				$"{nameof( _object )} : {_object}",
+				$"{nameof( parent )} : {parent}",
+				$"{nameof( lastTopDebug )} : {lastTopDebug}",
+				$"{nameof( _object._top )} : {_object._top}",
+				$"lastObjects :",
+				string.Join( "\n", lastTopDebug._objects._objects.Select( pair =>
+					$"    {pair.Key} : {pair.Value?.ToLineString()}" ) ),
+				$"{nameof( _object._objects )} :",
+				string.Join( "\n", _object._objects._objects.Select( pair =>
+					$"    {pair.Key} : {pair.Value?.ToLineString()}" ) )
+			) );
+			Log.Debug( $"{nameof( Run )} : end\n{this}" );
+#endif
 		}
+
 
 
 		public override string ToString() => base.ToString().InsertLast( ", ",
 			string.Join( ", ",
-				_parent?.name,
+				_parent?.name ?? "null",
 				_isWorldPositionStays
 			)
 			+ ", "
--- Assets/Test/Scripts/SMTask/SMObject/Modifyler/Linker/TestChangeParentSMObject.cs
+++ Assets/Test/Scripts/SMTask/SMObject/Modifyler/Linker/TestChangeParentSMObject.cs
@@ -14,6 +14,7 @@ namespace SubmarineMirage.TestSMTask.Modifyler {
 	using UTask;
 	using SMTask;
 	using SMTask.Modifyler;
+	using Scene;
 	using Debug;
 	using Test;
 
@@ -30,7 +31,14 @@ namespace SubmarineMirage.TestSMTask.Modifyler {
 			_createEvent.AddLast( async canceler => {
 				Log.Debug( $"start {nameof( Create )}{_testName}" );
 				switch ( _testName ) {
-					case nameof( TestSetParent ):	CreateTestSetParent();	break;
+					case nameof( TestSetParent1 ):			CreateTestSetParent1();				break;
+					case nameof( TestSetParent2 ):			CreateTestSetParent2();				break;
+					case nameof( TestChangeLink ):			CreateTestChangeLink();				break;
+					case nameof( TestUnLinkParent ):		CreateTestUnLinkParent();			break;
+					case nameof( TestReRegisterModifyler ):	CreateTestReRegisterModifyler();	break;
+					case nameof( TestChangeActive ):		CreateTestChangeActive();			break;
+					case nameof( TestError ):				CreateTestError();					break;
+						
 				}
 				Log.Debug( $"end {nameof( Create )}{_testName}" );
 
@@ -43,11 +51,11 @@ namespace SubmarineMirage.TestSMTask.Modifyler {
 		・親設定テスト
 		transform.SetParent、確認
 */
-		void CreateTestSetParent() {}
+		void CreateTestSetParent1() {}
 
 		[UnityTest] [Timeout( int.MaxValue )]
-		public IEnumerator TestSetParent() => From( async () => {
-			Log.Debug( $"{nameof( TestSetParent )}" );
+		public IEnumerator TestSetParent1() => From( async () => {
+			Log.Debug( $"{nameof( TestSetParent1 )}" );
 
 			var p = new GameObject( "1p" ).transform;
 			p.position = Vector3.one;
@@ -75,6 +83,235 @@ namespace SubmarineMirage.TestSMTask.Modifyler {
 
 			c = new GameObject( "5c" ).transform;
 			c.SetParent( null );
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・親設定テスト2
+		Run、transform.SetParent、確認
+*/
+		void CreateTestSetParent2() => TestSMBehaviourUtility.CreateBehaviours( @"
+			M1,
+			M1,
+			M1,
+			M1,
+			M2,
+				M2,
+			M2,
+				M2,
+		" );
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestSetParent2() => From( async () => {
+			Log.Debug( $"{nameof( TestSetParent2 )}" );
+
+
+			Log.Debug( "・子接続テスト" );
+			var p = SceneManager.s_instance.GetBehaviour<M1>()._object;
+			var c = p._next;
+			p._owner.transform.position = Vector3.one;
+			c._owner.transform.position = Vector3.one * 2;
+			c._top._modifyler.Register( new ChangeParentSMObject( c, p._owner.transform, true ) );
+			await c._top._modifyler.WaitRunning();
+
+			p = p._next;
+			c = p._next;
+			p._owner.transform.position = Vector3.one;
+			c._owner.transform.position = Vector3.one * 2;
+			c._top._modifyler.Register( new ChangeParentSMObject( c, p._owner.transform, false ) );
+			await c._top._modifyler.WaitRunning();
+
+
+			Log.Debug( "・子解除テスト" );
+			p = SceneManager.s_instance.GetBehaviour<M2>()._object;
+			c = p._child;
+			p._owner.transform.position = Vector3.one;
+			c._owner.transform.position = Vector3.one * 2;
+			c._top._modifyler.Register( new ChangeParentSMObject( c, null, true ) );
+			await c._top._modifyler.WaitRunning();
+
+			p = p._next;
+			c = p._child;
+			p._owner.transform.position = Vector3.one;
+			c._owner.transform.position = Vector3.one * 2;
+			c._top._modifyler.Register( new ChangeParentSMObject( c, null, false ) );
+			await c._top._modifyler.WaitRunning();
+
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・リンク変更テスト
+		Run、_object、parent、元_top、新_top、確認
+*/
+		void CreateTestChangeLink() => TestSMBehaviourUtility.CreateBehaviours( @"
+			M1,
+			M1,
+
+			M1,
+				null,
+			M3,
+
+			M1,
+				null,
+					null,
+			M6,
+
+
+			M1,
+				M1,
+			M1,
+
+			M1,
+				null,
+				M1,
+					null,
+			M3,
+
+			M1,
+				M1,
+				null,
+				null,
+					null,
+						null,
+					M1,
+			M6,
+		" );
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestChangeLink() => From( async () => {
+			Log.Debug( $"{nameof( TestChangeLink )}" );
+
+
+			Log.Debug( "・子接続テスト" );
+			var p = SceneManager.s_instance.GetBehaviour<M1>()._object;
+			var nextP = p._next._next;
+			var c = p._next;
+			c._top._modifyler.Register( new ChangeParentSMObject( c, p._owner.transform, true ) );
+			await c._top._modifyler.WaitRunning();
+
+			Log.Debug( "・b" );
+
+			p = nextP;
+			nextP = p._next;
+			c = SceneManager.s_instance.GetBehaviour<M3>()._object;
+			c._top._modifyler.Register( new ChangeParentSMObject( c, p._owner.transform.GetChild( 0 ), true ) );
+			await c._top._modifyler.WaitRunning();
+
+			Log.Debug( "・c" );
+
+			p = nextP;
+			nextP = p._next;
+			c = SceneManager.s_instance.GetBehaviour<M6>()._object;
+			c._top._modifyler.Register( new ChangeParentSMObject(
+				c, p._owner.transform.GetChild( 0 ).GetChild( 0 ), true ) );
+			await c._top._modifyler.WaitRunning();
+
+
+			Log.Debug( "・孫接続テスト" );
+			p = nextP;
+			nextP = p._next._next;
+			c = p._next;
+			c._top._modifyler.Register( new ChangeParentSMObject( c, p._child._owner.transform, true ) );
+			await c._top._modifyler.WaitRunning();
+
+			p = nextP;
+			nextP = p._next;
+			c = SceneManager.s_instance.GetBehaviour<M3>()._object;
+			c._top._modifyler.Register( new ChangeParentSMObject(
+				c, p._child._owner.transform.GetChild( 0 ), true ) );
+			await c._top._modifyler.WaitRunning();
+
+			p = nextP;
+			c = SceneManager.s_instance.GetBehaviour<M6>()._object;
+			c._top._modifyler.Register( new ChangeParentSMObject(
+				c, p._child._next._owner.transform.parent.GetChild( 0 ).GetChild( 0 ), true ) );
+			await c._top._modifyler.WaitRunning();
+
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・親解除テスト
+		Run、_object、parent、元_top、新_top、確認
+*/
+		void CreateTestUnLinkParent() => TestSMBehaviourUtility.CreateBehaviours( @"
+			M1,
+				M1,
+
+			M1,
+				M3,
+
+			M1,
+				M6,
+
+			M1,
+			M1,
+				M2,
+					M1,
+
+			M1,
+			M1,
+				M2,
+					M3,
+
+			M1,
+			M1,
+				M2,
+					M6,
+		" );
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestUnLinkParent() => From( async () => {
+			Log.Debug( $"{nameof( TestUnLinkParent )}" );
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・変更付け替えテスト
+		Run、溜まっている_modifylerの付け替え、確認
+*/
+		void CreateTestReRegisterModifyler() {}
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestReRegisterModifyler() => From( async () => {
+			Log.Debug( $"{nameof( TestReRegisterModifyler )}" );
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・活動状態変更テスト
+		Run、ChangeActiveSMObject、確認
+*/
+		void CreateTestChangeActive() {}
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestChangeActive() => From( async () => {
+			Log.Debug( $"{nameof( TestChangeActive )}" );
+
+			await UTask.Never( _asyncCanceler );
+		} );
+
+
+/*
+		・エラーテスト
+		生成時、SMBehaviourへ登録、null指定、Destroy済_parent指定、確認
+*/
+		void CreateTestError() {}
+
+		[UnityTest] [Timeout( int.MaxValue )]
+		public IEnumerator TestError() => From( async () => {
+			Log.Debug( $"{nameof( TestError )}" );
 
 			await UTask.Never( _asyncCanceler );
 		} );
